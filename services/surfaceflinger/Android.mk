8a4d4a969218533e0d55665b46ffdaa595cc920e
Anthony Cox <motor-head-1991@hotmail.com>
2015-10-21 03:36:23 -0600

    Use predefined paths to track headers
    Change-Id: Iac7381591e400eb59c81d0f9b137da9bc081dae6

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -103,8 +103,8 @@ LOCAL_SHARED_LIBRARIES := \
 	libpowermanager
 
 ifeq ($(TARGET_USES_QCOM_BSP), true)
-    LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
-    LOCAL_C_INCLUDES += hardware/qcom/display/libqdutils
+    LOCAL_C_INCLUDES += $(TOP)/$(call project-path-for,qcom-display)/libgralloc
+    LOCAL_C_INCLUDES += $(TOP)/$(call project-path-for,qcom-display)/libqdutils
     LOCAL_SHARED_LIBRARIES += libqdutils
     LOCAL_CFLAGS += -DQCOM_BSP
 endif
